42b4526f37dcaf0d4c5b563b561af40d9ced124d
Ishan7 <shnkarkee2@gmail.com>
2026-03-19 22:57:27 -0400

    Added the New Attributes-Section

--- Student.java
+++ Student.java
@@ -3,11 +3,17 @@ public class Student {
     int id;
     String firstName;
     String lastName;
+    String Section;
 
-    Student (int id; String firstName, String lastName){
+    Student (int id; String firstName, String lastName, String section){
         this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
+        this.section = section;
+    }
+
+    public Student(){
+
     }
 
 }
