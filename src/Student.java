eeef3e42aa4a0991fafacd8f292ec5201a364114
Pradeep <gpradeepnepal302@gmail.com>
2026-03-19 21:51:46 -0400

    Constructor, Getters, & Setters

--- src/Student.java
+++ src/Student.java
@@ -2,6 +2,35 @@ public class Student {
 
     int id;
     String firstName;
+    String lastName;
 
+    Student(int id, String firstName, String lastName){
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
 
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 }
