0cd865a97362b3be6f4fbbc604d17effd162152e
roni712 <ronakp765@gmail.com>
2026-03-19 22:47:28 -0300

    Added Pramater Constructor and default Construtor with last name

--- src/Student.java
+++ src/Student.java
@@ -2,6 +2,16 @@ public class Student {
 
     int id;
     String firstName;
+    String lastName;
+
+    public Student(int id,String firstName, String lastName){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.id = id;
+
+}
+public Student(){
+}
 
 
 }
