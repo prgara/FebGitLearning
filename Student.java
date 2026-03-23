0aeb06f4238f84b0a8e443042d88a2d3184e0e22
seyam-78c <seyam77050@gmail.com>
2026-03-23 16:35:15 -0400

    Add lastName and constructors to Student

--- Student.java
+++ Student.java
@@ -1,13 +1,16 @@
 public class Student {
 
     int id;
-    String firstName;
-    String lastName;
+    String firstName,lastName;
 
-    Student (int id; String firstName, String lastName){
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
+    public Student() {
+
+    }
+
+    public Student (int id; String firstName, String lastName){
+            this.id = id;
+            this.firstName = firstName;
+            this.lastName = lastName;
     }
 
 }
